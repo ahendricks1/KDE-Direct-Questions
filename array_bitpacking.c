b4e169bb893b884239ab530250ae04414b7f1704
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-27 16:52:25 -0700

    Implement pseudocode and write up for array bitpacking and fix minor grammar issues in README.

--- array_bitpacking.c
+++ array_bitpacking.c
@@ -1,6 +1,27 @@
 #include <stdint.h>
 
+
 int main(int argc, char *argv[]) {
 
+	int array[] = {1000, 1001, 1002, 1003}
+
+	bitPacking(array);
+
+	return 0;
+}
+
+int bitPacking(int array[]) {
+
+	int i;
+
+	//create new larger array to insert into
+
+	for(i = 0; i < sizeof(array) i++) {
+		// Bitmask the 12-bit integers
+		// Insert the bitmasked integers into the new larger array
+		// Shift the bits to the left 
+	}
+
+	// Return new larger array
 
 }
